1c53e4680f7f0b5dfd66459a08edbbca344dcc04
Mehmete28 <88893766+Mehmete28@users.noreply.github.com>
2026-05-15 15:56:32 +0300

    Add files via upload

---
+++ cs/e4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char s[] = "ABC";
+
+    printf("%c\n", s[0]);
+    printf("%c\n", s[1]);
+    printf("%c\n", s[2]);
+    printf("%d\n", s[3]);
+
+    return 0;
+}
